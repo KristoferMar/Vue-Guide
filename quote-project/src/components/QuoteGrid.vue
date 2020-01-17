2ab9dad80fb7ca9484091005eff9b4855d783ae0
Kristofer Mar Einarsson <kristofer.einarsson-cic@ibm.com>
2020-01-17 09:36:06 +0100

    Initial quote project commit

---
+++ quote-project/src/components/QuoteGrid.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="row">
+    <app-quote
+      v-for="(quote, index) in quotes"
+      @click.native="deleteQuote(index)"
+      v-bind:key="quote"
+    >{{ quote }}</app-quote>
+  </div>
+</template>
+
+<script>
+import Quote from './Quote.vue';
+
+export default {
+    props: ['quotes'],
+    components: {
+        appQuote: Quote,
+    },
+    methods: {
+        deleteQuote(index) {
+            this.$emit('quoteDeleted', index);
+        }
+    }
+}
+</script>
+
+<style lang="">
+    
+</style>
