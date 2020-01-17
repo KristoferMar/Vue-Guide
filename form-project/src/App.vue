aa96e49cc26c8a5b74d2e4584fc1f184b839160e
Kristofer Mar Einarsson <kristofer.einarsson-cic@ibm.com>
2020-01-17 11:26:49 +0100

    initial git commit for the form project where we do multiple form implementations

---
+++ form-project/src/App.vue
@@ -0,0 +1,149 @@
+<template>
+  <div class="container">
+    <form>
+      <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+          <h1>File a Complaint</h1>
+          <hr />
+          <div class="form-group">
+            <label for="email">Mail</label>
+            <!-- The v-model.lazy displays our input only after we tap away from the field -->
+            <input type="text" id="email" class="form-control" v-model="userData.email" />
+          </div>
+          <div class="form-group">
+            <label for="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              class="form-control"
+              v-model.lazy="userData.password"
+            />
+            <p>{{ userData.password }}</p>
+          </div>
+          <div class="form-group">
+            <label for="age">Age</label>
+            <input type="number" id="age" class="form-control" v-model="userData.age" />
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+          <label for="message">Message</label>
+          <br />
+          <!-- Interpolation between <textarea>{{ test }}</textarea> doesn't work!-->
+          <textarea id="message" rows="5" class="form-control" v-model="message"></textarea>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+          <div class="form-group">
+            <label for="sendmail">
+              <input type="checkbox" id="sendmail" value="SendMail" v-model="sendMail" /> Send Mail
+            </label>
+            <label for="sendInfomail">
+              <input type="checkbox" id="sendInfomail" value="SendInfoMail" v-model="sendMail" /> Send Infomail
+            </label>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+          <label for="male">
+            <input type="radio" id="male" value="Male" v-model="gender" /> Male
+          </label>
+          <label for="female">
+            <input type="radio" id="female" value="Female" v-model="gender" /> Female
+          </label>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group">
+          <label for="priority">Priority</label>
+          <select id="priority" class="form-control" v-model="selectedPriority">
+            <option v-for="priority in priorities" v-bind:key="priority">{{ priority }}</option>
+            <!-- :selected="priority == 'Medium'" : Could set a default selected value -->
+          </select>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group">
+          <app-switch v-model="dataSwitch"></app-switch>
+        </div>
+      </div>
+
+      <hr />
+      <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+          <!-- .prevent prevents us from sending form directly to server. We want to do something else -->
+          <button class="btn btn-primary" @click.prevent="submitted">Submit!</button>
+        </div>
+      </div>
+    </form>
+    <hr />
+    <div class="row" v-if="isSubmitted">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h4>Your Data</h4>
+          </div>
+          <div class="panel-body">
+            <p>Mail: {{ userData.email }}</p>
+            <p>Password: {{ userData.password }}</p>
+            <p>Age: {{ userData.age }}</p>
+            <!-- white-space: pre enables us to add linebreaks in the textarea -->
+            <p style="white-space: pre">Message: {{ message }}</p>
+            <p>
+              <strong>Send Mail?</strong>
+            </p>
+            <ul>
+              <li v-for="item in sendMail" v-bind:key="item">{{item}}</li>
+            </ul>
+            <p>Gender: {{ gender }}</p>
+            <p>Priority: {{ selectedPriority}}</p>
+            <p>Switched: {{ dataSwitch }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Switch from './components/Switch.vue';
+
+    export default {
+      data() {
+        return {
+          userData: {
+            email: '',
+            password: '',
+            age: 27,
+          },
+          message: 'A new Text',
+          //For the checkmarks
+          sendMail: [],
+          gender: 'Male',
+          selectedPriority: 'High',
+          priorities: ['High', 'Medium', 'Low'],
+          dataSwitch: true,
+          isSubmitted: false,
+        }
+      },
+      methods: {
+        submitted() {
+          this.isSubmitted = true;
+        }
+      },
+      components: {
+        appSwitch: Switch,
+      }
+    }
+</script>
+
+<style>
+
+</style>
