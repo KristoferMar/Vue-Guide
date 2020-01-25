7e24b6f4a926f6084e956ef5f5a8cb6604c4833c
KristoferMar <kristofer@brostu.dk>
2020-01-25 21:09:31 +0100

    How to make use of routing

--- vue-routing/src/components/user/UserEdit.vue
+++ vue-routing/src/components/user/UserEdit.vue
@@ -5,7 +5,7 @@
     <p>Locale: {{ $route.query.locale}}</p>
     <p>Analytics: {{ $route.query.q}}</p>
     <hr>
-    <button class="btn btn-primary" @click="confirmed = true">Confirm</button>
+    <button class="btn btn-primary" @click="confirmed  = true">Confirm</button>
     <div style="height: 700px"></div>
     <p id="data">Some extra data</p>
 </div>
