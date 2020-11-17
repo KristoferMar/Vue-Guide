c970667962f1f15f2c3f4c0b01ed3e40bbf8e1f1
Kristofer Mar Einarsson <kristofer@brostu.dk>
2020-11-17 12:15:38 +0100

    Extented the vue basics project to include a detailed overview of how we use the for-loop

--- vue-basics/src/App.vue
+++ vue-basics/src/App.vue
@@ -1,19 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+
+    <hr>
+
+    <ForLoop />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import ForLoop from "./components/ForLoop.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    ForLoop,
+  },
+};
 </script>
 
 <style>
---
+++ vue-basics/src/components/ForLoop.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <!-- In terms of syntax {{object.object}} is used directly between the html elements "<div>here</div>" -->
+    <!-- Elements in loop can also be used as html parameters as we see in the <li> tag with the use of ":" syntax. -->
+    <h2>Vue For-loop</h2>
+    <ul v-for="person in persons" :key="person">
+      <li :src=person.url>
+        name: {{ person.name }} <br />
+        age: {{ person.age }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "For-loop",
+  data() {
+    return {
+      persons: [
+        {
+          name: "Kristofer",
+          age: 25,
+          url: "This would be some kind of url"
+        },
+        {
+          name: "Henrik",
+          age: 25,
+        },
+        {
+          name: "Liam",
+          age: 32,
+        },
+        {
+          name: "Sofus",
+          age: 24,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
